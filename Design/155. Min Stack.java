dfc5c27b1d8c3663155e0f36b65a6a0d0ed5d66c
Jayasaravanakumar <thenkumar2001@gmail.com>
2026-03-17 12:19:12 +0530

    Min Stack.java

---
+++ Design/155. Min Stack.java
@@ -0,0 +1,43 @@
+class MinStack {
+       private Deque<Integer> mainStack;
+    private Deque<Integer> minStack;
+        
+
+    public MinStack() {
+        mainStack = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+        minStack.push(Integer.MAX_VALUE);
+       
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        minStack.push(Math.min(val, minStack.peek()));
+        
+    }
+    
+    public void pop() {
+        mainStack.pop();
+        minStack.pop();
+        
+    }
+    
+    public int top() {
+        return mainStack.peek();
+        
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+        
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
